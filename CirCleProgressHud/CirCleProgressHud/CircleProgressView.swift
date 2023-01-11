07799047e32c6ac87543ad15c37a37aa3ae019f6
Evan <jimmy7525@naver.com>
2023-01-12 00:13:54 +0900

    CircleProgressView 커스텀 뷰 구현

---
+++ CirCleProgressHud/CirCleProgressHud/CircleProgressView.swift
@@ -0,0 +1,70 @@
+//
+//  CircleProgressView.swift
+//  CirCleProgressHud
+//
+//  Created by 윤병일 on 2023/01/12.
+//
+
+import UIKit
+
+class CircleProgressView : UIView {
+  
+  // MARK: - Properties
+  static let shared = CircleProgressView()
+  
+  var circleLayer = CAShapeLayer()
+  var progressLayer = CAShapeLayer()
+  var startPoint = CGFloat(-Double.pi / 2)
+  var endPoint = CGFloat(3 * Double.pi / 2)
+  
+  // MARK: - Init
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    createCircularPath()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  // MARK: - Functions
+  func createCircularPath() {
+    let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height), radius: 20, startAngle: startPoint, endAngle: endPoint, clockwise: true)
+    
+    circleLayer.path = circularPath.cgPath
+    circleLayer.fillColor = UIColor.clear.cgColor
+    circleLayer.lineCap = .round
+    circleLayer.lineWidth = 10.0
+    circleLayer.strokeEnd = 1.0
+    circleLayer.strokeColor = UIColor.clear.cgColor
+    
+    layer.addSublayer(circleLayer)
+    
+    progressLayer.path = circularPath.cgPath
+    progressLayer.fillColor = UIColor.clear.cgColor
+    progressLayer.lineCap = .round
+    progressLayer.lineWidth = 5.0
+    progressLayer.strokeEnd = 0
+    progressLayer.strokeColor = UIColor.blue.cgColor
+    
+    layer.addSublayer(progressLayer)
+  }
+  
+  func startAnimation() {
+    let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
+    circularProgressAnimation.duration = 0.7
+    circularProgressAnimation.toValue = 1.0
+    circularProgressAnimation.fillMode = .forwards
+    circularProgressAnimation.isRemovedOnCompletion = false
+    circularProgressAnimation.repeatCount = 1000
+    progressLayer.add(circularProgressAnimation, forKey: "progressAnim")
+  }
+  
+  func stopAnimation() {
+    circleLayer.removeFromSuperlayer()
+    circleLayer.removeAllAnimations()
+    progressLayer.removeFromSuperlayer()
+    progressLayer.removeAllAnimations()
+  }
+}
